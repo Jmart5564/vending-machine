1524e910bef8af96ed461ee608f742889cf59ca8
Jmart5564 <Jmart5564@gmail.com>
2022-11-08 11:25:09 -0800

    setting up the constants

--- vending-machine/vending-machine-demo.ts
+++ vending-machine/vending-machine-demo.ts
@@ -6,11 +6,11 @@ let arg = null
 let cost = null
 let payment = null
 
-while((arg = process.argv.shift()) != null) {
-  if(arg == '--item-cost') {
+while ((arg = process.argv.shift()) != null) {
+  if (arg === '--item-cost') {
     const costInput = process.argv.shift()
     cost = Math.floor(Number(costInput || '0') * 100)
-  } else if(arg == '--payment') {
+  } else if (arg === '--payment') {
     const paymentInput = process.argv.shift()
     payment = Math.floor(Number(paymentInput || '0') * 100)
   }
@@ -20,11 +20,11 @@ console.error('--item-cost', cost)
 console.error('--payment', payment)
 
 // Narrow cost to a number.
-if(cost == null) {
+if (cost == null) {
   console.error('--item-cost is required but not provided. Exiting.')
   process.exit(1)
 }
-if(payment == null) {
+if (payment == null) {
   console.error('--payment is required but not provided. Exiting.')
   process.exit(2)
 }
--- vending-machine/vending-machine.ts
+++ vending-machine/vending-machine.ts
@@ -1,1 +1,17 @@
 // Implement your vending machine here!
+
+function vendingMachine (item: string, payment: number) {
+  const items = [
+    { name: 'Muffin', cost: 2.75 },
+    { name: 'Coffee', cost: 4.25 },
+    { name: 'Tea', cost: 1.55 },
+    { name: 'Scone', cost: 0.75 },
+    { name: 'Latte', cost: 2.24 }
+  ]
+
+  const chosenItem = items.find(vendingItem => vendingItem.name === item)
+
+  if (chosenItem) {
+    if (chosenItem.cost)
+  }
+}
