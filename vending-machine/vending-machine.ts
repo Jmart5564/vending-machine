5b9705bb3ecaf5602411115bf8bcfe1a3faccea5
Jmart5564 <Jmart5564@gmail.com>
2022-11-08 12:24:57 -0800

    added change const

--- vending-machine/vending-machine.ts
+++ vending-machine/vending-machine.ts
@@ -9,9 +9,53 @@ function vendingMachine (item: string, payment: number) {
     { name: 'Latte', cost: 2.24 }
   ]
 
-  const chosenItem = items.find(vendingItem => vendingItem.name === item)
+  const chosenItemWithEnoughMoney = items.find(vendingItem => vendingItem.name === item && payment >= vendingItem.cost)
 
-  if (chosenItem) {
-    if (chosenItem.cost)
+  const generateChange = change => {
+    change = (payment - chosenItemWithEnoughMoney.cost) * 100
+    let quarter = 0
+    let dime = 0
+    let nickel = 0
+    let penny = 0
+    let remainingChange = change
+
+    while (remainingChange >= 25) {
+      remainingChange -= 25
+      quarter++
+    }
+    while (remainingChange >= 10) {
+      remainingChange -= 10
+      dime++
+    }
+    while (remainingChange >= 5) {
+      remainingChange -= 5
+      nickel++
+    }
+    while (remainingChange > 0) {
+      remainingChange -= 1
+      penny++
+    }
+    if (quarter > 0) {
+      return `Quarters: ${quarter}`
+    }
+    if (dime > 0) {
+      return `Dimes: ${dime}`
+    }
+    if (nickel > 0) {
+      return `Nickels: ${nickel}`
+    }
+    if (penny > 0) {
+      return `Pennies: ${penny}`
+    } else {
+      return 'zero'
+    }
+  }
+
+  if (chosenItemWithEnoughMoney) {
+    return `Your ${item} has been served. Your change is ${generateChange}`
+  } else {
+    return `${item} doesn't exist or not enough money.`
   }
 }
+
+console.log(vendingMachine('Tea', 8))
