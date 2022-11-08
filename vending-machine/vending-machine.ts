776ac01b75a56d73ac465c868d8fa04573b1203b
Jmart5564 <Jmart5564@gmail.com>
2022-11-08 12:46:00 -0800

    refactored coe to be more simple, function is working

--- vending-machine/vending-machine.ts
+++ vending-machine/vending-machine.ts
@@ -1,61 +1,104 @@
 // Implement your vending machine here!
 
-function vendingMachine (item: string, payment: number) {
-  const items = [
-    { name: 'Muffin', cost: 2.75 },
-    { name: 'Coffee', cost: 4.25 },
-    { name: 'Tea', cost: 1.55 },
-    { name: 'Scone', cost: 0.75 },
-    { name: 'Latte', cost: 2.24 }
-  ]
+// function vendingMachine (item: string, payment: number) {
+//   const items = [
+//     { name: 'Muffin', cost: 2.75 },
+//     { name: 'Coffee', cost: 4.25 },
+//     { name: 'Tea', cost: 1.55 },
+//     { name: 'Scone', cost: 0.75 },
+//     { name: 'Latte', cost: 2.24 }
+//   ]
 
-  const chosenItemWithEnoughMoney = items.find(vendingItem => vendingItem.name === item && payment >= vendingItem.cost)
+//   const chosenItemWithEnoughMoney = items.find(vendingItem => vendingItem.name === item && payment >= vendingItem.cost)
 
-  const generateChange = change => {
-    change = (payment - chosenItemWithEnoughMoney.cost) * 100
-    let quarter = 0
-    let dime = 0
-    let nickel = 0
-    let penny = 0
-    let remainingChange = change
+//   const generateChange = change => {
+//     change = (payment - chosenItemWithEnoughMoney.cost) * 100
+//     let quarter = 0
+//     let dime = 0
+//     let nickel = 0
+//     let penny = 0
+//     let remainingChange = change
 
-    while (remainingChange >= 25) {
-      remainingChange -= 25
-      quarter++
-    }
-    while (remainingChange >= 10) {
-      remainingChange -= 10
-      dime++
-    }
-    while (remainingChange >= 5) {
-      remainingChange -= 5
-      nickel++
-    }
-    while (remainingChange > 0) {
-      remainingChange -= 1
-      penny++
-    }
-    if (quarter > 0) {
-      return `Quarters: ${quarter}`
-    }
-    if (dime > 0) {
-      return `Dimes: ${dime}`
-    }
-    if (nickel > 0) {
-      return `Nickels: ${nickel}`
-    }
-    if (penny > 0) {
-      return `Pennies: ${penny}`
-    } else {
-      return 'zero'
-    }
+//     while (remainingChange >= 25) {
+//       remainingChange -= 25
+//       quarter++
+//     }
+//     while (remainingChange >= 10) {
+//       remainingChange -= 10
+//       dime++
+//     }
+//     while (remainingChange >= 5) {
+//       remainingChange -= 5
+//       nickel++
+//     }
+//     while (remainingChange > 0) {
+//       remainingChange -= 1
+//       penny++
+//     }
+//     if (quarter > 0) {
+//       return `Quarters: ${quarter}`
+//     }
+//     if (dime > 0) {
+//       return `Dimes: ${dime}`
+//     }
+//     if (nickel > 0) {
+//       return `Nickels: ${nickel}`
+//     }
+//     if (penny > 0) {
+//       return `Pennies: ${penny}`
+//     } else {
+//       return 'zero'
+//     }
+//   }
+
+//   if (chosenItemWithEnoughMoney) {
+//     return `Your ${item} has been served. Your change is ${generateChange}`
+//   } else {
+//     return `${item} doesn't exist or not enough money.`
+//   }
+// }
+
+// console.log(vendingMachine('Tea', 8))
+
+function vendingMachine (item: number, payment: number) {
+  const change = Math.floor((payment - item) * 100)
+  let quarter = 0
+  let dime = 0
+  let nickel = 0
+  let penny = 0
+  let remainingChange = change
+  // declare object
+
+  while (remainingChange >= 25) {
+    remainingChange -= 25
+    quarter++
   }
-
-  if (chosenItemWithEnoughMoney) {
-    return `Your ${item} has been served. Your change is ${generateChange}`
-  } else {
-    return `${item} doesn't exist or not enough money.`
+  while (remainingChange >= 10) {
+    remainingChange -= 10
+    dime++
+  }
+  while (remainingChange >= 5) {
+    remainingChange -= 5
+    nickel++
+  }
+  while (remainingChange > 0) {
+    remainingChange -= 1
+    penny++
+  }
+  if (quarter > 0) {
+    console.log(`Quarters: ${quarter}`)
+  }
+  if (dime > 0) {
+    console.log(`Dimes: ${dime}`)
+  }
+  if (nickel > 0) {
+    console.log(`Nickels: ${nickel}`)
+  }
+  if (penny > 0) {
+    console.log(`Pennies: ${penny}`)
   }
 }
 
-console.log(vendingMachine('Tea', 8))
+vendingMachine(0.47, 0.52)
+
+// create format function
